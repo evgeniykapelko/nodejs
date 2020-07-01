6ce7b9af2d39bbf9172c325d41c013661fdb8b52
Evgeniy Kapelko <eugene.kapelko@balanceinternet.com.au>
2020-07-01 18:30:58 +0300

    Lesson 2. GET/POST, Express, prettier

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!!'))
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}))
+
+const arr = ['hello', 'world', 'test'];
+
+app.get('/', (req, res) => res.render('index', {arr: arr}));
+app.get('/create', (req, res) => res.render('create'));
+app.post('/create', (req, res) => {
+    arr.push(req.body.text);
+    res.redirect('/');
+});
+
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))

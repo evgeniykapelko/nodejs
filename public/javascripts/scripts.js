04149d32d163eba53837ae00e4b9ecda50f68c69
Evgeniy Kapelko <eugene.kapelko@balanceinternet.com.au>
2020-07-04 22:22:35 +0300

    Frontend, forms

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -1,1 +1,19 @@
-//hello js
+/* eslint-disable no-undef */
+$(function () {
+    // toggle
+    var flag = true;
+    $('.switch-button').on('click', function(e) {
+        e.preventDefault();
+
+        if (flag) {
+            flag = false;
+            $('.register').show('slow');
+            $('.login').hide();
+        } else {
+            flag = true;
+            $('.login').show('slow');
+            $('.register').hide();
+        }
+    });
+});
+/* eslint-enable no-undef */
